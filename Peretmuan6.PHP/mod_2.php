7db1ccf7894ed6673771fa1d2a9ac1d5b18ee8c7
aghissufiantoro <105733271+aghissufiantoro@users.noreply.github.com>
2023-03-13 03:54:27 +0700

    Pertemuan 6. PHP GET POST

---
+++ Peretmuan6.PHP/mod_2.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title> Pemrograman PHP dengan Array</title>
+</head>
+
+<body>
+    <?php
+    // Penulisan array dapat dibuat seperti berikut
+    $nama[] = "M. Aghis ";
+    $nama[] = "Sufiantoro ";
+    $nama[] = "Saputra ";
+    echo $nama[1] . $nama[2] . $nama[0];
+    echo "<br>";
+    ?>
+</body>
+
+</html>
